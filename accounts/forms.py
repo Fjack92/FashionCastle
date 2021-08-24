8ee8fd8ff837353948e2a4f317b84811da3f9e91
Fjack92 <30390328+Fjack92@users.noreply.github.com>
2021-08-24 19:28:01 -0400

    Dashboard Page

---
+++ accounts/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
+
+    error_css_class = "error"
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'password', 'email', 'confirm_password']
+    
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder']='Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder']='Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder']='Enter Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+    
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        password_length = len(password)
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords do not match"
+            )
+        elif password_length < 8:
+            raise forms.ValidationError(
+                "Passwords must be 8 characters or more"
+            )
+    
