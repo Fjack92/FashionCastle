92ad450ec48c59e254221c4c5a3d3198657d5c03
Fjack92 <30390328+Fjack92@users.noreply.github.com>
2021-08-25 20:07:15 -0400

    Order Details Page
    Co-authored-by: Tyler Antoni <Dibble710@users.noreply.github.com>

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from .models import Account, UserProfile
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from orders.models import Order
+from orders.models import Order, OrderProduct
 
 # verification imports
 from django.contrib.sites.shortcuts import  get_current_site
@@ -274,3 +274,17 @@ def change_password(request):
             messages.error(request, 'The two new passwords do not match!')
             return redirect('change_password')
     return render(request, 'accounts/change_password.html')
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal
+    }
+    return render(request, 'accounts/order_detail.html', context)
